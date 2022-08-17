55282b3ebce299c7a038b6df000e680dacdf1715
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-17 23:25:40 +0200

    jwt first part II

---
+++ middleware/authenticate.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const {UnauthenticatedError} = require('../errors')
+require('dotenv').config()
+
+const authenticate  = async (req,res,next)=>{
+    const authHeader = req.header.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new UnauthenticatedError('non autorisé veuillez réessayer une autre fois')
+    }
+    const token = authHeader.split(' ')[1]
+    try{
+        const decoded = await jwt.verify(token,process.env.JWT_SECRET)
+        req.user={userId:decoded.userId,role:decoded.role,nom:decoded.nom}
+        return next()
+    }
+    catch (err){
+        throw new UnauthenticatedError('non autorisé veuillez réessayer une autre fois')
+    }
+}
+
+module.exports=authenticate
