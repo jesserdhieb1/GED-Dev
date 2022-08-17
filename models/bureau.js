c12a7c90f1d239eab8facb64b19f006e16d21736
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-17 18:03:47 +0200

    bureau model

---
+++ models/bureau.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const {model} = require("mongoose");
+
+const bureauSchema = mongoose.Schema({
+    nomBureau:{
+        type:String,
+        required:[true,`svp entrez le nom de bureau`],
+        maxLength:20
+    },
+    adresse:{
+        type:String,
+        required:[true,`svp entrez l'adresse du bureau`],
+        maxLength:20
+    }
+})
+
+model.exports=mongoose.model('Bureau',bureauSchema)
