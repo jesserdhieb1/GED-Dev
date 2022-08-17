f27f72b728c1e966d6e99f9114a5788cf04e2146
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-17 19:10:41 +0200

    modif user model

--- models/user.js
+++ models/user.js
@@ -3,13 +3,32 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     nom:{
         type:String,
-        required:[true,`svp entrez le nom de l'utilisateur`],
+        required:[true,`svp entrez votre nom `],
         maxLength:20
     },
     prenom:{
         type:String,
-        required:[true,`svp entrez le prenom de l'utilisateur`],
+        required:[true,`svp entrez votre prenom `],
         maxLength:20
+    },
+    role:{
+        type:String,
+        enum:{
+            values:['ADMIN','PERSONNEL','ASSURANCE'],
+            message:`{VALUE} n'exist pas`
+        }
+    },
+    email:{
+        type:String,
+        require:[true,`svp entrez votre email`],
+        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            ,'please verify your email structure'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'please entrez votre mot de passe'],
+        minLength: 6
     }
 })
 
