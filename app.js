81113b1dc64c0a4a7eb00705f6660277af47ff54
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-17 06:16:43 +0200

    not found middleware

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express')
 const app = express()
 const connect  = require('./db/connect')
 const {StatusCode} = require('http-status-codes')
+const notFoundMiddleware = require('./middleware/not-found')
 
 
 app.get('/',(req,res)=>{
     res.status(StatusCode.OK).send('Hello :)')
 })
 
-
+app.use(notFoundMiddleware)
 
 const port = process.env.PORT || 5000
 const start = async()=>{
