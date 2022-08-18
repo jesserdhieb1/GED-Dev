428c1681aac4361d5e5dedab8e51d9eba0b2ffbd
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-18 03:29:45 +0200

    update bureau

--- controllers/bureau.js
+++ controllers/bureau.js
@@ -20,7 +20,7 @@ const deleteBureau=async (req,res)=>{
         const bureauId = req.params.id
         const bureau = await Bureau.findOneAndRemove({_id:bureauId})
         if (!bureau){
-            throw new BadRequestError('id de bureau incorrecte ')
+            throw new NotFoundError('id de bureau incorrecte ')
         }
         res.status(StatusCodes.OK).json({msg:'bureau supprimé'})
     }
@@ -29,4 +29,23 @@ const deleteBureau=async (req,res)=>{
     }
 }
 
-module.exports={createBureau,deleteBureau}
+const updateBureau=async (req,res)=>{
+    const role = req.user.role
+    if ( role==='ADMIN' ){
+        const bureauId = req.params.id
+        const {nomBureau,adresse} = req.body
+        if (!nomBureau || !adresse){
+            throw new BadRequestError('nom ou adresse bureau manquant ')
+        }
+        const bureau = await Bureau.findOneAndUpdate({_id:bureauId},req.body,{runValidators:true,new:true})
+        if (!bureau){
+            throw new NotFoundError('id de bureau incorrecte ')
+        }
+        res.status(StatusCodes.OK).json({bureau})
+    }
+    else {
+        throw new UnauthenticatedError('Role non autorisé ')
+    }
+}
+
+module.exports={createBureau,deleteBureau,updateBureau}
--- routes/bureau.js
+++ routes/bureau.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {createBureau,deleteBureau} = require('../controllers/bureau')
+const {createBureau,deleteBureau,updateBureau} = require('../controllers/bureau')
 
 router.route('/').post(createBureau)
-router.route('/:id').delete(deleteBureau)
+router.route('/:id').delete(deleteBureau).patch(updateBureau)
 
 
 module.exports=router
