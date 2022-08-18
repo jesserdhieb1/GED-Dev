9aa8d138c4ca676ce94854f175b94f5f11dc6373
jesser dhieb <dhieb.jesser@esprit.tn>
2022-08-18 02:47:05 +0200

    create bureau correction

--- controllers/bureau.js
+++ controllers/bureau.js
@@ -1,10 +1,18 @@
 const Bureau = require('../models/bureau')
 const {StatusCodes} = require("http-status-codes");
+const {UnauthenticatedError} = require('../errors')
 
 
 const createBureau=async (req,res)=>{
-    const bureau = await Bureau.create({...req.body});
-    return res.status(StatusCodes.CREATED).json({bureau})
+    const role = req.user.role
+    console.log(role)
+    if ( role==='ADMIN' || role==='PERSONNEL'){
+        const bureau = await Bureau.create({...req.body});
+        return res.status(StatusCodes.CREATED).json({bureau})
+    }
+    else {
+        throw new UnauthenticatedError('Role non autorisé ')
+    }
 }
 
 module.exports={createBureau}
